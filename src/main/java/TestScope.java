2a0c3252495cdaf5f3c6a80fefb847435b111502
anton-kashnikov <a.kashnikov23.trade@gmail.com>
2020-10-26 20:32:53 +0200

    Initial commit

---
+++ src/main/java/TestScope.java
@@ -0,0 +1,13 @@
+import javafx.application.*;
+import javafx.stage.Stage;
+
+
+public class TestScope extends Application {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.close();
+    }
+}

36a5aa697aa6210474fbfe2310bd0df96a6afd10
anton-kashnikov <a.kashnikov23.trade@gmail.com>
2020-10-26 20:40:39 +0200

    second change

--- src/main/java/TestScope.java
+++ src/main/java/TestScope.java
@@ -6,6 +6,7 @@ public class TestScope extends Application {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("first change");
+        System.out.println("second change");
     }
 
     public void start(Stage stage) throws Exception {

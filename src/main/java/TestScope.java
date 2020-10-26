722c81cb85c8f3883743b801ad0dc7c1a08262ce
anton-kashnikov <a.kashnikov23.trade@gmail.com>
2020-10-26 20:41:53 +0200

    branch-2 first change

--- src/main/java/TestScope.java
+++ src/main/java/TestScope.java
@@ -7,6 +7,7 @@ public class TestScope extends Application {
         System.out.println("Test");
         System.out.println("first change");
         System.out.println("second change");
+        System.out.println("branch-2 first change");
     }
 
     public void start(Stage stage) throws Exception {
